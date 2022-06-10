4366319b227bea7edcd1e45783a19ffe59fb1ef6
Prianka Ball <ball.prianka@gmail.com>
2022-06-10 03:24:17 -0400

    data

--- main/tw_process.py
+++ main/tw_process.py
@@ -44,6 +44,9 @@ def calculate_sentiments(df):
     df['scores'] = df['Text'].apply(lambda Text: sid.polarity_scores(Text))
     df['compound']  = df['scores'].apply(lambda score_dict: score_dict['compound'])
     df['comp_score'] = df['compound'].apply(lambda c: 'neu' if c ==0 else ('pos' if c>0 else 'neg'))
+    del df['Rendered Content']
+    del df['scores']
+    df = df.drop_duplicates(subset = ['Tweet Id'])
     return df
 
 
@@ -66,6 +69,10 @@ if __name__ == '__main__':
     after_superbowl_df = pd.concat((prep_concat(filenames_after_superbowl))).reset_index(drop=True)
 
     #calculate sentiments
-    calculate_sentiments(before_df).to_csv(r'preprocessed/before_df.csv')
-    calculate_sentiments(after_df).to_csv(r'preprocessed/after_df.csv')
-    calculate_sentiments(after_superbowl_df).to_csv(r'preprocessed/after_superbowl_df.csv')
+    before_df = calculate_sentiments(before_df)
+    after_df = calculate_sentiments(after_df)
+    after_superbowl_df = calculate_sentiments(after_superbowl_df)
+
+    before_df.to_csv(r'preprocessed/before_df.csv')
+    after_df.to_csv(r'preprocessed/after_df.csv')
+    after_superbowl_df.to_csv(r'preprocessed/after_superbowl_df.csv')
